437c8e565e62b9003baab50045bc966758b59de9
srtella <srinivasarao.tella@gmail.com>
2019-01-05 17:52:56 +0100

    refactored files

--- src/hospital-management-app.js
+++ src/hospital-management-app.js
@@ -31,9 +31,9 @@ setPassiveTouchGestures(true);
 // in `index.html`.
 setRootPath(MyAppGlobals.rootPath);
 
-class MyApp extends PolymerElement {
-  static get template() {
-    return html`
+class HospitalManagementApp extends PolymerElement {
+    static get template() {
+        return html `
       <style>
         :host {
           --app-primary-color: #4285f4;
@@ -109,65 +109,65 @@ class MyApp extends PolymerElement {
         </app-header-layout>
       </app-drawer-layout>
     `;
-  }
-
-  static get properties() {
-    return {
-      page: {
-        type: String,
-        reflectToAttribute: true,
-        observer: '_pageChanged'
-      },
-      routeData: Object,
-      subroute: Object
-    };
-  }
-
-  static get observers() {
-    return [
-      '_routePageChanged(routeData.page)'
-    ];
-  }
-
-  _routePageChanged(page) {
-     // Show the corresponding page according to the route.
-     //
-     // If no page was found in the route data, page will be an empty string.
-     // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
-    if (!page) {
-      this.page = 'view1';
-    } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
-      this.page = page;
-    } else {
-      this.page = 'view404';
     }
 
-    // Close a non-persistent drawer when the page & route are changed.
-    if (!this.$.drawer.persistent) {
-      this.$.drawer.close();
+    static get properties() {
+        return {
+            page: {
+                type: String,
+                reflectToAttribute: true,
+                observer: '_pageChanged'
+            },
+            routeData: Object,
+            subroute: Object
+        };
     }
-  }
 
-  _pageChanged(page) {
-    // Import the page component on demand.
-    //
-    // Note: `polymer build` doesn't like string concatenation in the import
-    // statement, so break it up.
-    switch (page) {
-      case 'view1':
-        import('./my-view1.js');
-        break;
-      case 'view2':
-        import('./my-view2.js');
-        break;
-      case 'view3':
-        import('./my-view3.js');
-        break;
-      case 'view404':
-        import('./my-view404.js');
-        break;
+    static get observers() {
+        return [
+            '_routePageChanged(routeData.page)'
+        ];
     }
-  }
+
+    _routePageChanged(page) {
+        // Show the corresponding page according to the route.
+        //
+        // If no page was found in the route data, page will be an empty string.
+        // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
+        if (!page) {
+            this.page = 'view1';
+        } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
+            this.page = page;
+        } else {
+            this.page = 'view404';
+        }
+
+        // Close a non-persistent drawer when the page & route are changed.
+        if (!this.$.drawer.persistent) {
+            this.$.drawer.close();
+        }
+    }
+
+    _pageChanged(page) {
+        // Import the page component on demand.
+        //
+        // Note: `polymer build` doesn't like string concatenation in the import
+        // statement, so break it up.
+        switch (page) {
+            case 'view1':
+                import ('./my-view1.js');
+                break;
+            case 'view2':
+                import ('./my-view2.js');
+                break;
+            case 'view3':
+                import ('./my-view3.js');
+                break;
+            case 'view404':
+                import ('./my-view404.js');
+                break;
+        }
+    }
 }
 
-window.customElements.define('my-app', MyApp);
+window.customElements.define('hospital-management-app', HospitalManagementApp);
